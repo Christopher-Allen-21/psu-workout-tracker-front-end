90a4cab364c1cde9400d460157bab81f746024f6
Christopher Allen <callen7908@yahoo.com>
2025-02-01 18:02:20 -0500

    Delete works. Still need to work on POST/PUT

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const app = express()
 dotenv.config()
 
 const PORT = process.env.PORT || 7000
-const MONGOURL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL
 
 app.use(express.json())
 
-mongoose.connect(MONGOURL).then(() => {
+mongoose.connect(MONGO_URL).then(() => {
     console.log("Db is connected successfully")
     app.listen(PORT, () => {
         console.log(`Server is running on http://${PORT}`)
@@ -34,10 +34,10 @@ app.get('/users', async(req, res) => {
         res.status(200).send({
             user
         })
-    }
-    catch {
+    } 
+    catch (error) {
         res.status(500).send({
-            message: "An error occurred"
+            message: `An error occurred: ${error}`
         })
     }
 })
@@ -60,20 +60,60 @@ app.get('/users/:id', async(req, res) => {
 
 
 
-app.post('/users/:id', (req, res) => {
-    const user = req.body
-
-    if(!res.body) {
+app.post('/users', async(req, res) => {
+    
+    if(!req.body) {
         res.status(400).send({
-            message: 'No User provided'
+            message: "Bad request. No user provided."
+        }) 
+    }
+    else {
+        const newUser = new UserModel({ ...req.body })
+
+        try {
+            const insertedUser = await newUser.save()
+            res.status(201).json(insertedUser)
+        }
+        catch {
+            res.status(500).send({
+                 message: `An error occurred: ${error}`
+            })
+        }
+    } 
+})
+    
+app.put('/users/:id', async(req, res) => {
+    const id = req.params.id
+
+    try {
+        await UserModel.updateOne(id, req.body)
+        const updatedUser = await UserModel.findById(id)
+        res.status(200).send({
+            updatedUser
         })
     }
-
-    res.status(201).send({
-        tshirt: `tshirt with your logo: ${logo} and ID of ${id}`
-    })
+    catch (error) {
+        res.status(404).send({
+            message: `User not found ${error}`
+        })
+    }
 })
 
+    
+app.delete('/users/:id', async(req, res) => {
+    const id = req.params.id
+
+    try {
+        const userToDelete = await UserModel.findById(id)
+        await UserModel.deleteOne(userToDelete)
+        res.status(200).send()
+    }
+    catch (error) {
+        res.status(500).send({
+            message: `An error occurred: ${error}`
+       })
+    }
+})
 
 
 /*
