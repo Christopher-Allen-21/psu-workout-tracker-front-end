7b8ea04a49188e117ba6ba7ad54387a91cd3bd7e
Christopher Allen <callen7908@yahoo.com>
2025-02-09 21:10:28 -0500

    Added functionality to create/update a user

--- client/src/app/user-profile/user-profile.component.ts
+++ client/src/app/user-profile/user-profile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'user-profile',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss',
 })
@@ -13,6 +14,21 @@ export class UserProfileComponent {
   baseUrl = 'http://localhost:3000/'
 
   users: User[] = []
+
+  firstNameInput: string
+  lastNameInput: string
+  birthDateInput: string
+  emailInput: string
+  heightInput: number
+  weightInput: number
+
+  updateIdInput: string
+  updateFirstNameInput: string
+  updateLastNameInput: string
+  updateBirthDateInput: string
+  updateEmailInput: string
+  updateHeightInput: number
+  updateWeightInput: number
 
 
   constructor(private readonly httpClient: HttpClient) {}
@@ -50,9 +66,44 @@ export class UserProfileComponent {
     return user
   }
 
-  updateUser(id: string): void {
-    console.log("hi")
-    alert("Suck it bitch" + id)
+  createUser(): void {
+    let url = this.baseUrl + 'users/'
+
+    let request: User = {
+      _id: null,
+      firstName: this.firstNameInput,
+      lastName: this.lastNameInput,
+      birthDate: this.birthDateInput,
+      email: this.emailInput,
+      height: this.heightInput,
+      weight: this.weightInput
+    }
+
+    this.httpClient.post(url, request, {observe: 'response'}).subscribe({
+      next: (res) => {
+        alert("Response status code: " + JSON.stringify(res.status) + "\nUser created successfully.")
+      }
+    })
+  }
+
+  updateUser(): void {
+    let url = this.baseUrl + 'users/' + this.updateIdInput + '/'
+
+    let request: User = {
+      _id: this.updateIdInput,
+      firstName: this.updateFirstNameInput,
+      lastName: this.updateLastNameInput,
+      birthDate: this.updateBirthDateInput,
+      email: this.updateEmailInput,
+      height: this.updateHeightInput,
+      weight: this.updateWeightInput
+    }
+
+    this.httpClient.put(url, request, {observe: 'response'}).subscribe({
+      next: (res) => {
+        alert("Response status code: " + JSON.stringify(res.status) + "\nUser " + this.updateIdInput + " updated successfully.")
+      }
+    })
   }
 
   deleteUser(id: string): void {
