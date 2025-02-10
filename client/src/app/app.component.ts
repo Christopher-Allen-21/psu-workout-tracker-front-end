582418b1216ce7dfd6d0be3b752ec027cc27177b
Christopher Allen <callen7908@yahoo.com>
2025-02-09 20:05:34 -0500

    Added routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MatTabsModule } from '@angular/material/tabs'
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserProfileComponent, MatTabsModule],
+  imports: [RouterOutlet, RouterLink, UserProfileComponent, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
+  routerLinks: string[] = [
+    'workout',
+    'programs',
+    'user-profile'
+  ]
+
+
+
+
 }
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { WorkoutComponent } from './workout/workout.component';
+import { ProgramsComponent } from './programs/programs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'workout', component: WorkoutComponent
+    },
+    {
+        path: 'programs', component: ProgramsComponent
+    },
+    {
+        path: 'user-profile', component: UserProfileComponent
+    }
+];
