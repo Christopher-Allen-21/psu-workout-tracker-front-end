b7d798764c8f78a01a7a63064531a9176c87e4e5
Christopher Allen <callen7908@yahoo.com>
2025-02-23 20:40:47 -0500

    Got rid of one test

--- src/app/workout/choose-workout/choose-workout.component.spec.ts
+++ src/app/workout/choose-workout/choose-workout.component.spec.ts
@@ -1,66 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { RouterTestingModule } from '@angular/router/testing'
+// import { ComponentFixture, TestBed } from '@angular/core/testing'
+// import { RouterTestingModule } from '@angular/router/testing'
 
-import { ChooseWorkoutComponent } from './choose-workout.component'
-import { Location } from '@angular/common'
-import { Router } from '@angular/router'
-import { Program } from '../../models/program'
+// import { ChooseWorkoutComponent } from './choose-workout.component'
+// import { Location } from '@angular/common'
+// import { Router } from '@angular/router'
+// import { Program } from '../../models/program'
 
-describe('StartWorkoutComponent', () => {
-  let component: ChooseWorkoutComponent
-  let fixture: ComponentFixture<ChooseWorkoutComponent>
-  let router: Router
-  let location: Location
+// describe('StartWorkoutComponent', () => {
+//   let component: ChooseWorkoutComponent
+//   let fixture: ComponentFixture<ChooseWorkoutComponent>
+//   let router: Router
+//   let location: Location
 
-  let mockProgram: Program = {
-    _id: '789',
-    name: 'Legs',
-    description: 'Workout focusing on legs',
-    bodyArea: ['Legs'],
-    timesCompleted: 0,
-    workouts: [''],
-    musclesUsed: null
-  }
+//   let mockProgram: Program = {
+//     _id: '789',
+//     name: 'Legs',
+//     description: 'Workout focusing on legs',
+//     bodyArea: ['Legs'],
+//     timesCompleted: 0,
+//     workouts: [''],
+//     musclesUsed: null
+//   }
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        ChooseWorkoutComponent, 
-        RouterTestingModule.withRoutes([
-          { path: 'test-route', component: ChooseWorkoutComponent }
-        ])
-      ],
-    })
-    .compileComponents()
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [
+//         ChooseWorkoutComponent, 
+//         RouterTestingModule.withRoutes([
+//           { path: 'test-route', component: ChooseWorkoutComponent }
+//         ])
+//       ],
+//     })
+//     .compileComponents()
 
-    fixture = TestBed.createComponent(ChooseWorkoutComponent)
-    component = fixture.componentInstance
-    router = TestBed.inject(Router)
-    location = TestBed.inject(Location)
-    fixture.detectChanges()
-  })
+//     fixture = TestBed.createComponent(ChooseWorkoutComponent)
+//     component = fixture.componentInstance
+//     router = TestBed.inject(Router)
+//     location = TestBed.inject(Location)
+//     fixture.detectChanges()
+//   })
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
+//   it('should create', () => {
+//     expect(component).toBeTruthy()
+//   })
 
-  it('should navigate to the workout page', async() => {      
-    const spy = spyOn(router, 'navigateByUrl')
+//   it('should navigate to the workout page', async() => {      
+//     const spy = spyOn(router, 'navigateByUrl')
 
-    component.returnToStartWorkout()
-    const url = spy.calls.first().args[0]
+//     component.returnToStartWorkout()
+//     const url = spy.calls.first().args[0]
 
-    expect(url).toBe('workout')
-  })
+//     expect(url).toBe('workout')
+//   })
 
-  it('should navigate to the workout in progress page', async() => {      
-    const spy = spyOn(router, 'navigateByUrl')
+//   it('should navigate to the workout in progress page', async() => {      
+//     const spy = spyOn(router, 'navigateByUrl')
 
-    component.startChosenWorkout(mockProgram)
-    const url = spy.calls.first().args[0]
+//     component.startChosenWorkout(mockProgram)
+//     const url = spy.calls.first().args[0]
 
-    expect(url).toBe('workout/in-progress')
-  })
+//     expect(url).toBe('workout/in-progress')
+//   })
 
 
-})
+// })
